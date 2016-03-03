0b271340939ea79a42e5cefac69384a991703e88
incepted <mintception@gmail.com>
2016-03-02 19:17:23 -0500

    Refresh

--- SyncAdapterLab/app/src/main/java/com/example/wasabi/syncadapterlab/MainActivity.java
+++ SyncAdapterLab/app/src/main/java/com/example/wasabi/syncadapterlab/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.wasabi.syncadapterlab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mButton1, mButton2, mButton3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton1 = (Button)findViewById(R.id.button_manual);
+        mButton2 = (Button)findViewById(R.id.button_minute);
+        mButton3 = (Button)findViewById(R.id.button_fiveminutes);
+
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
